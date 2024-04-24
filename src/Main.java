8bf117ea972b3da28228bbb10cc609f9087c87cc
Javohir004 <javohirsharifjonov795@gmail.com>
2024-04-24 11:24:32 +0500

    bu task2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
+
         CustomLinkedList<Integer> test = new CustomLinkedList<>();
         test.add(1);
         test.add(2);
@@ -10,5 +11,6 @@ public class Main {
         System.out.println(test.get(2));
         test.remove(1);
         System.out.println(test.get(1));
+
     }
 }
