8d1b1402da7c8fb286b268e2030846a30d2273b4
Javohir004 <javohirsharifjonov795@gmail.com>
2024-04-24 12:24:47 +0500

    bu task4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,45 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world !!!");
+        Set<String> test1 = new HashSet<>();
+        test1.add("olma");
+        test1.add("behi");
+        test1.add("nok");
+        test1.add("tuxum");
+        test1.add("ayiq");
+
+        Set<String> test2 = new HashSet<>();
+        test2.add("olma");
+        test2.add("shaftoli");
+        test2.add("nok");
+        test2.add("daraxt");
+        test2.add("bo'ri");
+
+        Set<String> test3 = new HashSet<>();
+        test3.add("olma");
+        test3.add("shaftoli");
+        test3.add("nok");
+        test3.add("daraxt");
+
+
+
+        Set<String> last = new HashSet<>();
+
+        for (String s : test1) {
+            if( test2.contains(s)){
+                last.add(s);
+            }
+        }
+
+        for (String s3 : test3) {
+            if( !last.contains(s3) ){
+                last.add(s3);
+            }
+        }
+
+        System.out.println(last);
+
     }
 }
