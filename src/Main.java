1ecef2676e0f120fbe9fb3c5638e4c079747238e
Javohir004 <javohirsharifjonov795@gmail.com>
2024-04-26 15:13:28 +0500

    voah

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,7 @@ import java.util.Set;
 public class Main {
     public static void main(String[] args) {
 
-        CustomLinkedList<Integer> test = new CustomLinkedList<>();
-        test.add(1);
-        test.add(2);
-        test.add(3);
-        test.add(8);
-        test.add(5);
-        System.out.println(test.get(2));
-        test.remove(1);
-        System.out.println(test.get(1));
+        Task2();
 
         Set<String> test1 = new HashSet<>();
         test1.add("olma");
@@ -53,5 +45,18 @@ public class Main {
 
         System.out.println(last);
 
+    }
+
+        /** Task 2 niki **/
+    private static void Task2() {
+        CustomLinkedList<Integer> test = new CustomLinkedList<>();
+        test.add(1);
+        test.add(2);
+        test.add(3);
+        test.add(8);
+        test.add(5);
+        System.out.println(test.get(2));
+        test.remove(1);
+        System.out.println(test.get(1));
     }
 }
